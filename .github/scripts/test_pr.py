37f9c90b39104d68e07e2b20b6582c7f8d9f63a7
jambii1 <s.maslevtsov@gmail.com>
2026-05-01 14:10:38 +0300

    [master] remove trailing whitespaces in scripts and workflows

--- .github/scripts/test_pr.py
+++ .github/scripts/test_pr.py
@@ -11,11 +11,11 @@ def check_pr_title(title: str) -> re.match:
 
 def main():
   title = str(os.getenv("PR_TITLE"))
-  
+
   if not title:
     print("Incorrect script arguments")
     sys.exit(1)
-  
+
   if not check_pr_title(title):
     print("Pr title must begin with [client], [daemon], [master] or [tests]")
     sys.exit(1)
